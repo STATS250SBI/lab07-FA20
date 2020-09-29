fc77f5c592f9679ca17a872d01e300047ba66dca
nseewald1 <nseewald1@gmail.com>
2020-09-29 13:38:55 -0400

    initial commit

---
+++ auxiliary.R
@@ -0,0 +1,121 @@
+#Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+#incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur
+#ridiculus mus mauris vitae ultricies. Egestas egestas fringilla phasellus
+#faucibus scelerisque eleifend donec pretium. Bibendum at varius vel pharetra
+#vel turpis. Imperdiet nulla malesuada pellentesque elit eget gravida. Neque
+#vitae tempus quam pellentesque nec nam aliquam sem et. Neque vitae tempus quam
+#pellentesque nec nam aliquam sem. Fringilla est ullamcorper eget nulla facilisi
+#etiam. At augue eget arcu dictum varius duis at consectetur lorem. Posuere
+#morbi leo urna molestie at elementum eu. Suspendisse in est ante in nibh
+#mauris. Semper quis lectus nulla at. Ut pharetra sit amet aliquam id diam
+#maecenas ultricies mi. Aliquam ut porttitor leo a diam sollicitudin tempor id.
+#Augue interdum velit euismod in pellentesque. Egestas sed tempus urna et.
+#Tempor id eu nisl nunc mi ipsum. Morbi tincidunt augue interdum velit. Neque
+#volutpat ac tincidunt vitae semper quis lectus nulla at. Lectus vestibulum
+#mattis ullamcorper velit sed.
+#
+# Sed blandit libero volutpat sed cras ornare arcu. In hac habitasse platea
+# dictumst vestibulum rhoncus est pellentesque elit. Eget arcu dictum varius
+# duis at consectetur lorem donec. Venenatis urna cursus eget nunc scelerisque.
+# Cursus mattis molestie a iaculis at. Montes nascetur ridiculus mus mauris
+# vitae ultricies leo. Justo laoreet sit amet cursus sit amet dictum. Cursus
+# vitae congue mauris rhoncus aenean vel. Quis hendrerit dolor magna eget est
+# lorem ipsum. Massa id neque aliquam vestibulum morbi.
+#
+# Amet dictum sit amet justo donec enim diam. Sit amet porttitor eget dolor
+# morbi non. Pharetra sit amet aliquam id diam maecenas ultricies mi eget. In
+# nulla posuere sollicitudin aliquam ultrices. Et tortor consequat id porta
+# nibh. Donec ac odio tempor orci. Donec adipiscing tristique risus nec feugiat
+# in fermentum. Tellus id interdum velit laoreet. Aliquam eleifend mi in nulla
+# posuere sollicitudin. Adipiscing tristique risus nec feugiat. Massa eget
+# egestas purus viverra accumsan in. Fermentum leo vel orci porta non. Aliquam
+# ut porttitor leo a diam sollicitudin tempor id eu. Posuere lorem ipsum dolor
+# sit amet consectetur.
+#
+# Mattis vulputate enim nulla aliquet porttitor. Purus faucibus ornare
+# suspendisse sed nisi lacus sed. Sem et tortor consequat id porta nibh
+# venenatis. Id faucibus nisl tincidunt eget nullam non. Ut consequat semper
+# viverra nam libero justo laoreet sit. Velit laoreet id donec ultrices. Vivamus
+# at augue eget arcu dictum varius duis at consectetur. Pharetra magna ac
+# placerat vestibulum lectus mauris ultrices. Eget mauris pharetra et ultrices
+# neque ornare aenean euismod. Suscipit tellus mauris a diam maecenas sed.
+# Lectus urna duis convallis convallis tellus id interdum velit. Pellentesque id
+# nibh tortor id aliquet lectus proin. Elit ullamcorper dignissim cras tincidunt
+# lobortis feugiat vivamus at augue. Viverra nam libero justo laoreet sit amet.
+#
+# Facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat. Id eu
+# nisl nunc mi ipsum faucibus vitae aliquet nec. Dictum at tempor commodo
+# ullamcorper a. Faucibus vitae aliquet nec ullamcorper sit amet. Lorem ipsum
+# dolor sit amet consectetur adipiscing elit duis tristique. Quam id leo in
+# vitae turpis massa sed elementum. Lorem ipsum dolor sit amet consectetur
+# adipiscing elit ut. Ac tortor dignissim convallis aenean et tortor at risus.
+# Neque convallis a cras semper. Convallis convallis tellus id interdum. Amet
+# justo donec enim diam. Pellentesque nec nam aliquam sem et tortor consequat id
+# porta. Viverra justo nec ultrices dui sapien eget mi. Netus et malesuada fames
+# ac turpis. Eu turpis egestas pretium aenean pharetra magna ac placerat
+# vestibulum. Tempor orci dapibus ultrices in iaculis. Fermentum odio eu feugiat
+# pretium nibh. Amet consectetur adipiscing elit duis. Cras sed felis eget velit
+# aliquet sagittis id.
+
+flipCoin <- function(n) {
+  sample(c("Heads", "Tails"), size = n, replace = T, prob = c(.55, .45))
+}
+
+#Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+#incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur
+#ridiculus mus mauris vitae ultricies. Egestas egestas fringilla phasellus
+#faucibus scelerisque eleifend donec pretium. Bibendum at varius vel pharetra
+#vel turpis. Imperdiet nulla malesuada pellentesque elit eget gravida. Neque
+#vitae tempus quam pellentesque nec nam aliquam sem et. Neque vitae tempus quam
+#pellentesque nec nam aliquam sem. Fringilla est ullamcorper eget nulla facilisi
+#etiam. At augue eget arcu dictum varius duis at consectetur lorem. Posuere
+#morbi leo urna molestie at elementum eu. Suspendisse in est ante in nibh
+#mauris. Semper quis lectus nulla at. Ut pharetra sit amet aliquam id diam
+#maecenas ultricies mi. Aliquam ut porttitor leo a diam sollicitudin tempor id.
+#Augue interdum velit euismod in pellentesque. Egestas sed tempus urna et.
+#Tempor id eu nisl nunc mi ipsum. Morbi tincidunt augue interdum velit. Neque
+#volutpat ac tincidunt vitae semper quis lectus nulla at. Lectus vestibulum
+#mattis ullamcorper velit sed.
+#
+# Sed blandit libero volutpat sed cras ornare arcu. In hac habitasse platea
+# dictumst vestibulum rhoncus est pellentesque elit. Eget arcu dictum varius
+# duis at consectetur lorem donec. Venenatis urna cursus eget nunc scelerisque.
+# Cursus mattis molestie a iaculis at. Montes nascetur ridiculus mus mauris
+# vitae ultricies leo. Justo laoreet sit amet cursus sit amet dictum. Cursus
+# vitae congue mauris rhoncus aenean vel. Quis hendrerit dolor magna eget est
+# lorem ipsum. Massa id neque aliquam vestibulum morbi.
+#
+# Amet dictum sit amet justo donec enim diam. Sit amet porttitor eget dolor
+# morbi non. Pharetra sit amet aliquam id diam maecenas ultricies mi eget. In
+# nulla posuere sollicitudin aliquam ultrices. Et tortor consequat id porta
+# nibh. Donec ac odio tempor orci. Donec adipiscing tristique risus nec feugiat
+# in fermentum. Tellus id interdum velit laoreet. Aliquam eleifend mi in nulla
+# posuere sollicitudin. Adipiscing tristique risus nec feugiat. Massa eget
+# egestas purus viverra accumsan in. Fermentum leo vel orci porta non. Aliquam
+# ut porttitor leo a diam sollicitudin tempor id eu. Posuere lorem ipsum dolor
+# sit amet consectetur.
+#
+# Mattis vulputate enim nulla aliquet porttitor. Purus faucibus ornare
+# suspendisse sed nisi lacus sed. Sem et tortor consequat id porta nibh
+# venenatis. Id faucibus nisl tincidunt eget nullam non. Ut consequat semper
+# viverra nam libero justo laoreet sit. Velit laoreet id donec ultrices. Vivamus
+# at augue eget arcu dictum varius duis at consectetur. Pharetra magna ac
+# placerat vestibulum lectus mauris ultrices. Eget mauris pharetra et ultrices
+# neque ornare aenean euismod. Suscipit tellus mauris a diam maecenas sed.
+# Lectus urna duis convallis convallis tellus id interdum velit. Pellentesque id
+# nibh tortor id aliquet lectus proin. Elit ullamcorper dignissim cras tincidunt
+# lobortis feugiat vivamus at augue. Viverra nam libero justo laoreet sit amet.
+#
+# Facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat. Id eu
+# nisl nunc mi ipsum faucibus vitae aliquet nec. Dictum at tempor commodo
+# ullamcorper a. Faucibus vitae aliquet nec ullamcorper sit amet. Lorem ipsum
+# dolor sit amet consectetur adipiscing elit duis tristique. Quam id leo in
+# vitae turpis massa sed elementum. Lorem ipsum dolor sit amet consectetur
+# adipiscing elit ut. Ac tortor dignissim convallis aenean et tortor at risus.
+# Neque convallis a cras semper. Convallis convallis tellus id interdum. Amet
+# justo donec enim diam. Pellentesque nec nam aliquam sem et tortor consequat id
+# porta. Viverra justo nec ultrices dui sapien eget mi. Netus et malesuada fames
+# ac turpis. Eu turpis egestas pretium aenean pharetra magna ac placerat
+# vestibulum. Tempor orci dapibus ultrices in iaculis. Fermentum odio eu feugiat
+# pretium nibh. Amet consectetur adipiscing elit duis. Cras sed felis eget velit
+# aliquet sagittis id.
